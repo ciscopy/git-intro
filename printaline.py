47f9904e40f06db2127c386b2b3ec2052e381c6d
Alfredo Ramos <alfrramo@cisco.com>
2016-09-11 12:17:25 -0500

    commit from master

--- printaline.py
+++ printaline.py
@@ -1,4 +1,6 @@
-"""This file was copied from another lab..."""
+"""This file was copied from another lab...
+    something else
+    """
 
 def take(count, iterable):
     """Take items from the front of an iterable
